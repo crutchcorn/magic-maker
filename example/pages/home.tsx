7e9e5d4ae1cb3cd6ac29bad6c0fee354bb6ba12c
Corbin Crutchley <git@crutchcorn.dev>
2022-05-21 06:23:23 -0700

    chore: demonstrate why js-parser won't work as we intend it to

--- example/pages/home.tsx
+++ example/pages/home.tsx
@@ -3,6 +3,23 @@ import React, { useId } from "react";
 import CSSComp from "../pageComponents/home.css";
 import { BodyScript, HeadScript } from "../../src/components/script";
 import useUniqueId from "../../src/hooks/useUniqueId";
+import { unwrapFunctionBody } from "../../src/js-parser";
+
+function alertTest() {
+  alert("Test");
+}
+
+function addHelloClickButton(id: string) {
+  // This doesn't work because `id` is transformed to a minified result (say, `t`), but is left within the template literal
+  // When the function is stringified. This means that, without additional compiler work (like a webpack plugin), this
+  // will never work as we expect it to.
+  return () => {
+    const el = document.querySelector(`#${id}`);
+    el!.addEventListener("click", () => {
+      alert("HELLO");
+    });
+  };
+}
 
 const Home = () => {
   const id = useUniqueId();
@@ -11,18 +28,9 @@ const Home = () => {
     <>
       <CSSComp />
       <BodyScript
-        contents={`
-            const el = document.querySelector("#${id}");
-            el.addEventListener('click', () => {
-                alert("HELLO");
-            })
-        `}
+        contents={unwrapFunctionBody(addHelloClickButton(id).toString())}
       />
-      <HeadScript
-        contents={`
-        alert("Test");
-      `}
-      />
+      <HeadScript contents={unwrapFunctionBody(alertTest.toString())} />
       <h1 id={id}>Home</h1>
     </>
   );
