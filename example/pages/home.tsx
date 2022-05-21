b94a8b16153a471df51f922da139351e91fe027f
Corbin Crutchley <git@crutchcorn.dev>
2022-05-21 06:37:30 -0700

    chore: demo usage of `unwrapFunctionBody` properly

--- example/pages/home.tsx
+++ example/pages/home.tsx
@@ -3,6 +3,18 @@ import React, { useId } from "react";
 import CSSComp from "../pageComponents/home.css";
 import { BodyScript, HeadScript } from "../../src/components/script";
 import useUniqueId from "../../src/hooks/useUniqueId";
+import { unwrapFunctionBody } from "../../src/js-parser";
+
+function alertTest() {
+  alert("Test");
+}
+
+function addHelloClickButton() {
+  const el = document.querySelector(`#__ID__`);
+  el!.addEventListener("click", () => {
+    alert("HELLO");
+  });
+}
 
 const Home = () => {
   const id = useUniqueId();
@@ -11,18 +23,12 @@ const Home = () => {
     <>
       <CSSComp />
       <BodyScript
-        contents={`
-            const el = document.querySelector("#${id}");
-            el.addEventListener('click', () => {
-                alert("HELLO");
-            })
-        `}
+        contents={unwrapFunctionBody(addHelloClickButton.toString()).replace(
+          "__ID__",
+          id
+        )}
       />
-      <HeadScript
-        contents={`
-        alert("Test");
-      `}
-      />
+      <HeadScript contents={unwrapFunctionBody(alertTest.toString())} />
       <h1 id={id}>Home</h1>
     </>
   );
